ac315cab78b24e7774e48070bda38f9a0d18a05c
Subhransu-Swain <94684980+Subhransu-Swain@users.noreply.github.com>
2022-07-19 16:32:44 +0530

    Add files via upload

---
+++ javascript/external.js
@@ -0,0 +1,7 @@
+function myfunction()
+        {
+            // used for single line
+            document.write("Subhransu Swain");
+            /*used for 
+            muliple line */
+        }
